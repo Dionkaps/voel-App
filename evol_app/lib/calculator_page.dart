553472042f95f3bd0b62df5dd19a9301f34f9a7f
Dionisis Konstantinos Kapsimalis <vpddkf@gmail.com>
2023-03-09 12:50:55 +0200

    Oloklirwsi diaxeirisis dedomenwn API

--- evol_app/lib/calculator_page.dart
+++ evol_app/lib/calculator_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CalcPage extends StatefulWidget {
   @override
@@ -6,19 +8,83 @@ class CalcPage extends StatefulWidget {
 }
 
 class _CalcPageState extends State<CalcPage> {
-  
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
   bool isEnabled = true;
-  
+  bool state2 = true;
+  double percentageValue = 0;
+
   @override
   Widget build(BuildContext context) {
+    Future<void> apiRes(name1, name2) async {
+      final headers = {
+        'X-RapidAPI-Key': '38e6466c77mshca67ff4a6f2b640p191cdfjsn63ace31c29a8',
+        'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+      };
+      final response = await http.get(
+          Uri.parse(
+              'https://love-calculator.p.rapidapi.com/getPercentage?sname=$name1&fname=$name2'),
+          headers: headers);
+
+      if (response.statusCode == 200) {
+        //The status code 200 means the request has succeeded
+        final Map<String, dynamic> data = json.decode(response.body);
+        final percentage = data['percentage'];
+        final result = data['result'];
+
+        print(percentage + result);
+
+        setState(() {
+          percentageValue = double.parse(percentage);
+        });
+      } else {
+        throw Exception('Failed to load data');
+        // Handle the error
+      }
+    }
+
+    void toggleWidgets() {
+      state2 = !state2;
+    }
+
+    Widget buttonGo = ElevatedButton(
+      child: const Text('GO'),
+      onPressed: () {
+        String name1 = _controller1.text;
+        String name2 = _controller2.text;
+        apiRes(name1, name2);
+        setState(() {
+          toggleWidgets();
+          isEnabled = false;
+        });
+      },
+    );
+
+    Widget percentage = Container(
+      child: Text('$percentageValue'),
+    );
+
+    Widget restart = ElevatedButton(
+      child: const Text('Calculate New'),
+      onPressed: () {
+        setState(() {
+          state2 = !state2;
+          isEnabled = true;
+          _controller1.text = '';
+          _controller2.text = '';
+          percentageValue = 0;
+        });
+      },
+    );
+
+    Widget justText = Text("");
+
     double screenWidth = MediaQuery.of(context).size.width;
     double tfPadding = 50.0;
+    Widget displayWidget = state2 ? buttonGo : percentage;
+    Widget displayWidget1 = state2 ? justText : restart;
 
     return Scaffold(
-      appBar: AppBar(
-        //automaticallyImplyLeading: false, // Hides the back arrow button
-        title: const Text('Magic Page'),
-      ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -28,10 +94,12 @@ class _CalcPageState extends State<CalcPage> {
               child: SizedBox(
                 width: screenWidth * 0.7,
                 child: TextField(
+                  controller: _controller1,
                   enabled: isEnabled,
                   decoration: InputDecoration(
                     hintText: "Person1",
-                    contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 5),
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
                     filled: true,
                     fillColor: Colors.blue.shade100,
                     border: OutlineInputBorder(
@@ -43,24 +111,19 @@ class _CalcPageState extends State<CalcPage> {
               ),
             ),
             Center(
-              child: ElevatedButton(
-                child: const Text('GO'),
-                onPressed: () {
-                  setState(() {
-                    isEnabled = false;
-                  });
-                },
-              ),
+              child: displayWidget,
             ),
             Padding(
               padding: EdgeInsets.only(top: tfPadding),
               child: SizedBox(
                 width: screenWidth * 0.7,
                 child: TextField(
+                  controller: _controller2,
                   enabled: isEnabled,
                   decoration: InputDecoration(
                     hintText: "Person1",
-                    contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 5),
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
                     filled: true,
                     fillColor: Colors.red.shade100,
                     border: OutlineInputBorder(
@@ -71,6 +134,10 @@ class _CalcPageState extends State<CalcPage> {
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: tfPadding),
+              child: displayWidget1,
+            )
           ]),
     );
   }
